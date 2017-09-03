34ba6f60b190707324b2106bd362f29b29b3298c
kotoyuuko <kotoyuuko@gmail.com>
2017-09-03 11:00:43 +0800

    Add upload controller

---
+++ app/Http/Controllers/UploadController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use GuzzleHttp\Client;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+
+class UploadController extends Controller
+{
+    //
+}
