ca759c0fbc8f4a7c4eda9626a6e1c10830d486ec
kotoyuuko <kotoyuuko@gmail.com>
2017-09-03 11:04:20 +0800

    finish upload func

--- app/Http/Controllers/UploadController.php
+++ app/Http/Controllers/UploadController.php
@@ -8,5 +8,55 @@ use Symfony\Component\HttpFoundation\File\UploadedFile;
 
 class UploadController extends Controller
 {
-    //
+    public function upload(Request $request)
+    {
+        $validator = \Validator::make($request->all(), [
+            'file' => 'required|max:5096|image'
+        ]);
+
+        if ($validator->fails()) {
+            return $this->fail($validator->errors()->first());
+        }
+
+        $file = $request->file;
+
+        try {
+            $url = $this->uploadToSMMS($file)['data']['url'];
+            return $this->success($url);
+        } catch (\Exception $e) {
+            return $this->fail($e->getMessage());
+        }
+    }
+
+    /**
+     * Upload image to sm.ms
+     *
+     * @param  UploadedFile $file
+     * @return array
+     */
+    private function uploadToSMMS($file)
+    {
+        $client = new Client();
+
+        $response = $client->post('https://sm.ms/api/upload', [
+            'multipart' => [
+                [
+                    'name' => 'smfile',
+                    'contents' => fopen($file->getRealPath(), 'r'),
+                    'filename' => $file->getClientOriginalName()
+                ]
+            ]
+        ]);
+
+        return json_decode($response->getBody()->getContents(), true);
+    }
+
+    public function success($data = [], $code = 200)
+    {
+        return ['code' => $code, 'data' => $data];
+    }
+    public function fail($data = [], $code = 500)
+    {
+        return ['code' => $code, 'data' => $data];
+    }
 }
